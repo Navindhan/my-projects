06b6a557e5e2f37877bbf9df02f9a6fa199228b3
Navindhan <66838441+Navindhan@users.noreply.github.com>
2021-03-09 02:48:12 -0800

    error identification added

--- flames.js
+++ flames.js
@@ -18,37 +18,44 @@ btn.addEventListener('click',function(e){
 		var name1=user.value;		
 		var name2=crush.value;		
 		var error=0;
+		var rare=0;
 		
-		// var except=['!','@','#','$','%','^','&','*','(',')','-','_','+','=',']','"',"'",';',':','{','[','}','|','\\',',','.','?','/','>','<'];
+		var except=['!','@','#','$','%','^','&','*','(',')','-','_','+','=',']','"',"'",';',':',
+		'{','[','}','|','\\',',','.','?','/','>','<','0','1','2','3','4','5','6','7','8','9'];
 		
 		for(i=0;i<name1.length;i++){				
-			name1=name1.replace(" ","")								
+			name1=name1.replace(" ","");								
 		}	
 		for(i=0;i<name2.length;i++){				
-			name2=name2.replace(" ","")								
+			name2=name2.replace(" ","");								
 		}
 		name1=name1.toLowerCase();
 		name2=name2.toLowerCase();			
-		
-		var extra_name = name1;
-		
+
+		var ext1=name1;
+		var ext2=name2;
+
 		if(((name1=="maarimuthu") && (name2=="muthumaari")) || 
 			((name2=="maarimuthu") && (name1=="muthumaari")) ||
 			 ((name1=="marimuthu") && (name2=="muthumari")) || 
 			 ((name2=="marimuthu") && (name1=="muthumari"))){
-			error=1;
+				rare=1;
 		}
 
-		// for(i=0;i<name1.length;i++){
-		// 	for(j=0;j<except.length;i++){
-		// 		if(name1[i]==except[j]){
-		// 			error=1;
-		// 			console.log("error");
-		// 		}
-		// 	}
-		// }
-					
-		
+		for(i=0;i<name1.length;i++){
+			for(j=0;j<except.length;j++){
+				if(name1[i]==except[j]){
+					error=1;					
+				}
+			}
+		}
+		for(i=0;i<name2.length;i++){
+			for(j=0;j<except.length;j++){
+				if(name2[i]==except[j]){
+					error=1;					
+				}
+			}
+		}
 
 		var f_list=[["friends1", 1.17], ["friends2", 1], ["friends3", 1.5], ["friends4", 1.11], ["friends5", 1.505]];
 		var l_list=[["lovers1", 0.998], ["lovers2", 1.34], ["lovers3", 1], ["lovers4", 1.33], ["lovers5", 1.78]];
@@ -57,25 +64,43 @@ btn.addEventListener('click',function(e){
 		var e_list=[["enemies1", 1.27], ["enemies2", 1.5], ["enemies3", 1], ["enemies4", 1.11]];
 		var s_list=[["siblings1", 0.67], ["siblings2", 1], ["siblings3", 1]];
 		
-		var c_list=[["counter", 1.78]];
+		var c_list=[["counter1", 1.78], ["counter2", 1.16], ["counter3", 1], ["counter4", 1.33], ["counter5", 1.33]];
 		
 		var images=[{"result" : "Friends", "image" : f_list}, {"result" : "Lovers", "image" : l_list},
 		 {"result" : "Affectionate", "image" : a_list}, {"result" : "Marriage", "image" : m_list},
 		  {"result" : "Enemies", "image" : e_list}, {"result" : "Siblings", "image" : s_list}, {
 		  	"result" : "Counter", "image" : c_list
 		  }];			 
-		for(i=0;i<name1.length;i++){
-			for(j=0;j<name2.length;j++){					
-				if(name1[i]==name2[j]){						
-					name1=name1.replace(name1[i],"");
-					name2=name2.replace(name2[j],"");						
+		for(i=0; i < name1.length;i++){
+			for(j=0;j < name2.length;j++){									
+				if(name2[j]==name1[i]){					
+					name1 = name1.replace(name1[i], "");
+					name2 = name2.replace(name2[j], "");											
 				}
 			}
 		}
-
+		
 		user.value="";
 		crush.value="";
-		var count=(name1+name2).length;			
+		var count=(name1+name2).length;
+
+		var a=0;		
+		
+		if(name1.length==name2.length){
+			for(i=0; i < name1.length;i++){
+				for(j=0;j < name2.length;j++){									
+					if(name1[i]==name2[j]){
+						a++;
+					}
+				}
+			}
+			if(a==(count/2)){
+				count=0;
+			}					
+
+		}
+		
+
 		if(count>0){
 			var list1=["Friends","Lovers","Affectionate","Marriage","Enemies","Siblings"];
 			var rand=Math.floor(Math.random()*(list1.length));								
@@ -108,31 +133,83 @@ btn.addEventListener('click',function(e){
 				}
 			}			
 		}
+		else if(count==0){
+			list1="Counter";
+			for(i=0;i<images.length;i++){
+				if(images[i]["result"]==list1){
+					var var1 = (images[i]["image"]);
+					var rand=4;					
+				}
+			}
+			list1="Fill correct names";			
+		}
 		
-		if(extra_name=="pugazenthi"){
+		if(ext1=='pugazenthi'){
 			list1="Marriage";
 		}
-			
+		
 		for(i=0;i<images.length;i++){
 			if(images[i]["result"]==list1){
 				var var1 = (images[i]["image"]);
 				var rand=Math.floor(Math.random()*(var1.length));					
 			}
 		}
-		if(error==1){
+		if(ext1=="navindhan" || (ext1=="navindhan" && ext2=="navindhan")){
 			list1="Counter";
 			for(i=0;i<images.length;i++){
 				if(images[i]["result"]==list1){
 					var var1 = (images[i]["image"]);
-					var rand=Math.floor(Math.random()*(var1.length));					
+					var rand=1;					
 				}
 			}
-			list1="Very rare Couple";
-			flamesBox.innerHTML="<img src='"+var1[rand][0]+".jpeg'><h1 class='final'>"+list1+"</h1>";	
+			list1="Use Your name not mine";			
 		}
+		
+		else if(ext2=="navindhan"){
+			list1="Counter";
+			for(i=0;i<images.length;i++){
+				if(images[i]["result"]==list1){
+					var var1 = (images[i]["image"]);
+					var rand=2;					
+				}
+			}
+			list1="Who is that lucky girl!";
+			flamesBox.innerHTML="<img src='"+var1[rand][0]+".jpeg'>"+"<h1 class='final'>"+list1+"</h1>";	
+				
+		}
+		else if(rare==1){
+			list1="Counter";
+			for(i=0;i<images.length;i++){
+				if(images[i]["result"]==list1){
+					var var1 = (images[i]["image"]);
+					var rand=0;					
+				}
+			}			
+			list1="Very rare Couple";
+		}
+		else if(error==1){
+			list1="Counter";
+			for(i=0;i<images.length;i++){
+				if(images[i]["result"]==list1){
+					var var1 = (images[i]["image"]);
+					var rand=4;					
+				}
+			}			
+			list1="Use only Name letters not other letters";
+		}
+
+		if(list1=="Who is that lucky girl!"){
+			setTimeout(function(){
+				rand=3;
+				list1="Lovers";
+				flamesBox.innerHTML="<img src='"+var1[rand][0]+".jpeg'>"+"<h1 class='final'>"+list1+"</h1>";				
+			}, 3000);
+		}
+
 		else{
 			flamesBox.innerHTML="<img src='"+var1[rand][0]+".jpeg'>"+"<h1 class='final'>"+list1+"</h1>";
 		}
+		
 		var old_width=400;
 		var new_width=var1[rand][1]*300;
 		if(new_width < old_width){
@@ -141,8 +218,9 @@ btn.addEventListener('click',function(e){
 		}
 		else{
 			flamesBox.style.width=new_width+'px';				
-		}
-		const db_list=list1.toString();
+		}	
+
+		const db_list=list1.toString();				
 		database.ref('/users2/'+db_id).set({
 			a_userName: dbname1,
 			b_crushName: dbname2,
